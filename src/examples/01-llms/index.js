413b554bb5aa62e10ca55e3c9dbf12657d0d9ba2
heora <yueluo.yang@gmail.com>
2023-07-09 09:44:02 +0800

    feat: llms memory and agents

--- src/examples/01-llms/index.js
+++ src/examples/01-llms/index.js
@@ -1,21 +1,33 @@
 import { OpenAI } from 'langchain/llms/openai'
+
 import { PromptTemplate } from 'langchain/prompts'
 import { LLMChain } from 'langchain/chains'
 
-import { OPENAI_API_KEY } from '../../config/index.js'
+import { initializeAgentExecutorWithOptions } from 'langchain/agents'
+import { SerpAPI } from 'langchain/tools'
+import { Calculator } from 'langchain/tools/calculator'
 
-const model = new OpenAI({ openAIApiKey: OPENAI_API_KEY, temperature: 0.9 })
+import { BufferMemory } from 'langchain/memory'
+import { ConversationChain } from 'langchain/chains'
 
-const insertToHtml = htmlStr
+import { OPENAI_API_KEY, SERPAPI_API_KEY } from '../../config/index.js'
 
+const insert = htmlStr => (document.body.innerHTML = htmlStr)
+
+// llms
 const case01 = async () => {
+  const model = new OpenAI({ openAIApiKey: OPENAI_API_KEY, temperature: 0.9 })
   const res = await model.call('一家生产彩色袜子的公司起什么名字好？')
 
   console.log(res)
-  document.body.innerHTML = res
+
+  insert(res)
 }
 
+// prompt and chains
 const case02 = async () => {
+  const model = new OpenAI({ openAIApiKey: OPENAI_API_KEY, temperature: 0.9 })
+
   const template = '一家生产{product}的公司起什么名字好？'
   const prompt = new PromptTemplate({ template, inputVariables: ['product'] })
 
@@ -26,13 +38,56 @@ const case02 = async () => {
 
   console.log(res)
 
-  document.body.i
+  insert(res.text)
+}
+
+// agents
+const case03 = async () => {
+  const model = new OpenAI({ openAIApiKey: OPENAI_API_KEY, temperature: 0 })
+  const tools = [
+    new SerpAPI(SERPAPI_API_KEY, {
+      location: 'Austin,Texas,United States',
+      hl: 'en',
+      gl: 'us'
+    }),
+    new Calculator()
+  ]
+
+  const executor = await initializeAgentExecutorWithOptions(tools, model, {
+    agentType: 'zero-shot-react-description'
+  })
+
+  console.log('Loaded agent.')
+
+  const input =
+    "Who is Olivia Wilde's boyfriend?" +
+    ' What is his current age raised to the 0.23 power?'
+  console.log(`Executing with input "${input}"...`)
+
+  const result = await executor.call({ input })
+
+  console.log(`Got output ${result.output}`)
+}
+
+// memory
+const case04 = async () => {
+  const model = new OpenAI({ openAIApiKey: OPENAI_API_KEY })
+  const memory = new BufferMemory()
+
+  const chain = new ConversationChain({ llm: model, memory })
+
+  const res1 = await chain.call({ input: '你好，我是 heora。' })
+  console.log(res1)
+
+  const res2 = await chain.call({ input: '我的名字是什么' })
+  console.log(res2)
 }
 
 const App = async () => {
   // await case01()
-
-  await case02()
+  // await case02()
+  // await case03()
+  // await case04()
 }
 
 App()
---
+++ src/examples/02-chat-models/index.js
@@ -0,0 +1,7 @@
+import {} from 'langchain/chat_models/openai'
+
+const insert = htmlStr => (document.body.innerHTML = htmlStr)
+
+const App = async () => {}
+
+App()
