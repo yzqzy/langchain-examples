f8833adb4e84e619c337f31071078dcda1b395fb
heora <yueluo.yang@gmail.com>
2023-07-11 05:56:42 +0800

    perf: update

--- src/examples/04-nodejs/index.js
+++ src/examples/04-nodejs/index.js
@@ -4,7 +4,12 @@ const { HumanMessage } = require('langchain/schema')
 const { OPENAI_API_KEY } = require('../../config/index.module')
 
 const App = async () => {
-  const chat = new ChatOpenAI({ temperature: 0, openAIApiKey: OPENAI_API_KEY })
+  const chat = new ChatOpenAI(
+    { temperature: 0, openAIApiKey: OPENAI_API_KEY },
+    {
+      // basePath: 'http://localhost:7890'
+    }
+  )
   const response = await chat.call([
     new HumanMessage(
       'Translate this sentence from English to French. I love programming.'
