58567ce5cb6eb3028f590df3bc0828a740ad97dc
heora <yangzhiqiang@winwin-inc.com>
2023-07-10 18:33:37 +0800

    feat: add nodejs example

---
+++ src/examples/04-nodejs/index.js
@@ -0,0 +1,16 @@
+const { ChatOpenAI } = require('langchain/chat_models/openai')
+const { HumanMessage } = require('langchain/schema')
+
+const { OPENAI_API_KEY } = require('../../config/index.module')
+
+const App = async () => {
+  const chat = new ChatOpenAI({ temperature: 0, openAIApiKey: OPENAI_API_KEY })
+  const response = await chat.call([
+    new HumanMessage(
+      'Translate this sentence from English to French. I love programming.'
+    )
+  ])
+  console.log(response)
+}
+
+App()
