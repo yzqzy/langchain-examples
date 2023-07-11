00aa36c0e334ac3acb9508015d94e363272bd15b
heora <yangzhiqiang@winwin-inc.com>
2023-07-11 09:52:17 +0800

    docs: windows git bash 设置终端代理

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # langchain-examples
 
+git bash 终端代理
+
+```bash
+export http_proxy=http://127.0.0.1:7890
+export https_proxy=http://127.0.0.1:7890
+```
--- src/examples/04-nodejs/index.js
+++ src/examples/04-nodejs/index.js
@@ -4,18 +4,19 @@ const { HumanMessage } = require('langchain/schema')
 const { OPENAI_API_KEY } = require('../../config/index.module')
 
 const App = async () => {
-  const chat = new ChatOpenAI(
-    { temperature: 0, openAIApiKey: OPENAI_API_KEY },
-    {
-      // basePath: 'http://localhost:7890'
-    }
-  )
-  const response = await chat.call([
-    new HumanMessage(
-      'Translate this sentence from English to French. I love programming.'
+  try {
+    const chat = new ChatOpenAI(
+      { temperature: 0, openAIApiKey: OPENAI_API_KEY }
     )
-  ])
-  console.log(response)
+    const response = await chat.call([
+      new HumanMessage(
+        'Translate this sentence from English to French. I love programming.'
+      )
+    ])
+    console.log(response)
+  } catch (error) {
+    console.log(error.message)
+  }
 }
 
 App()
