8a9d4c0d4b336aaa85c6317d1f6506af2ff6a3b9
heora <yangzhiqiang@winwin-inc.com>
2023-07-12 10:08:05 +0800

    perf: 样例规范调整

---
+++ src/examples/06-chat-dashboard/pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="wrap">
+    <h2>langchain examples</h2>
+
+      <NuxtLink class="nav" to="/givename">llms - give your a name</NuxtLink>
+      <NuxtLink class="nav" to="/translate">chat models - translation assistant</NuxtLink>
+      <NuxtLink class="nav" to="/chat">chat - data analysis</NuxtLink>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.wrap {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  .nav {
+    margin-top: 15px;
+  }
+
+}
+</style>
