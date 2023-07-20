dec5d4d9354dd1ed4b4a79dd9e1d8ff7aa3b5773
heora <yangzhiqiang@winwin-inc.com>
2023-07-20 16:44:02 +0800

    docs: adjust case

--- src/examples/08-chat-custom-api/index.js
+++ src/examples/08-chat-custom-api/index.js
@@ -7,9 +7,14 @@ const App = async () => {
   const url = `http://ml-jsonrpc.banmahui.cn/chat/completion?apikey=${process.env.MSY_API_KEY}`
 
   const { data } = await axios.post(url, {
-    question: '元气森林2023年的销售额'
+    question: '只统计 便利店业态，再全国范围内，可口可乐品牌在2022年的品牌销量趋势是怎样的？',
+    history: [
+      {
+        role: 'system',
+        content: `If {Input} contains drawing, use vega-lite syntax to return the drawing JSON schema, which is as rich as possible as possible.`
+      }
+    ]
   })
-
   console.log(data)
 }
 
