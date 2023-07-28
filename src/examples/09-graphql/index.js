c109f4f47dcf2e0816d3e6d25a21b383ecaa6ebc
heora <yangzhiqiang@winwin-inc.com>
2023-07-28 11:10:27 +0800

    perf: schema analysis test

--- src/examples/09-graphql/index.js
+++ src/examples/09-graphql/index.js
@@ -1,4 +1,7 @@
+import fs from 'fs'
+
 import { graphql, buildSchema } from 'graphql'
+import gql from 'graphql-tag'
 
 const testCase1 = () => {
   const schema = buildSchema(`
@@ -15,8 +18,48 @@ const testCase1 = () => {
   })
 }
 
+const testCase2 = () => {
+  const query = gql`
+    {
+      user(id: 5) {
+        firstName
+        lastName
+      }
+    }
+  `
+  console.log(query)
+}
+
+const testCase3 = () => {
+  const query = gql`
+    ${fs.readFileSync('./schema.graphql', 'utf8')}
+  `
+
+  const type_definiations = query.definitions.filter(
+    item => item.kind === 'ObjectTypeDefinition' && item.name.value !== 'Query'
+  )
+
+  // const timerSeries = ty
+
+  while (type_definiations.length) {
+    const definitions = type_definiations.shift()
+
+    definitions.fields.forEach(item => {
+      const { type } = item
+
+      if (type.kind === 'NamedType' && type.name.value === 'Date') {
+        console.log(item)
+      }
+    })
+  }
+
+  // console.log(type_definiations)
+}
+
 const App = () => {
-  testCase1()
+  // testCase1()
+  // testCase2()
+  testCase3()
 }
 
 App()
