ac9ba1d4317ee617050adc3436c9ae4d92edca62
heora <yueluo.yang@gmail.com>
2023-07-29 17:05:40 +0800

    feat: add apollo client fetch example

---
+++ src/examples/11-apollo-client/app.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>{{ animals }}</div>
+</template>
+
+<script lang="ts" setup>
+const animals =  ref()
+
+const query = gql`
+  query Animals {
+    animals {
+      ... on Spider {
+        name
+        footLength
+      }
+      ... on Bird {
+        name
+        footLength
+        wingLength
+        wing
+      }
+    }
+  }
+`
+
+const { data } = await useAsyncQuery(query)
+animals.value = data.value
+</script>
